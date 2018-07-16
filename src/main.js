5d245aeec0b4f9c52008e4902a88da27652ee1cf
Khundmeer <khundmeerfahad@gmail.com>
2018-07-16 16:00:31 -0500

    Added image and application window

--- src/main.js
+++ src/main.js
@@ -37,3 +37,29 @@ if(!PIXI.utils.isWebGLSupported()){
 }
 
 PIXI.utils.sayHello(type)
+
+var w = document.width;
+var h = document.height;
+var appWidth = 600;
+
+//Create a Pixi Application
+let app = new PIXI.Application({width: appWidth, height: 100, antialias: true});
+
+//let app = new PIXI.Application({width: 500, height: 50});
+//Add the canvas that Pixi automatically created for you to the HTML document
+console.log("doc", document);
+app.view.style.left = (w/2);
+app.view.style.position = "relative";
+
+document.body.appendChild(app.view);
+
+app.renderer.autoResize = true;
+
+
+//resizing the application window
+//app.renderer.resize(512, 512);
+
+//If I want to change the background color of the application
+//app.renderer.backgroundColor = 0x061639;
+
+let texture = PIXI.utils.TextureCache["CharGlobie.jpeg"];
