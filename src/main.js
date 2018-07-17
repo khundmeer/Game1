5c7f68c32803667601e1e39da5a8b53d5878843e
Khundmeer <khundmeerfahad@gmail.com>
2018-07-17 12:51:41 -0500

    Tried to make the screen in the middle

--- src/main.js
+++ src/main.js
@@ -18,7 +18,7 @@ var Game =
     
     currentscore: function()
     {
-
+        
 
     },
 
@@ -30,30 +30,71 @@ var Game =
     High_Score: 0
 }
 
-
+//This links pixi to our JS file
 let type = "WebGL"
 if(!PIXI.utils.isWebGLSupported()){
     type = "canvas"
 }
 
 PIXI.utils.sayHello(type)
+var w = screen.width;
+var h = screen.height;
 
-//find out how to get the width and height of the window ????????????????????????????????????????????
-var w = document.width;
-var h = document.height;
-var appWidth = 600;
 
-//Create a Pixi Application
-let app = new PIXI.Application({width: appWidth, height: 100, antialias: true});
 
-//let app = new PIXI.Application({width: 500, height: 50});
-//Add the canvas that Pixi automatically created for you to the HTML document
+//window.alert(h);
+//console.log(h);
 
-app.view.style.left = (w/2) - (appWidth/2);
-app.view.style.position = "relative";
+//Main Screen of the game
+var AppWidth = 500;
+var AppHeight = 100;
+//window.alert(h);
+
+let app = new PIXI.Application({width: AppWidth, height: AppHeight, antialias: true});
+app.renderer.backgroundColor = 0x000000; //Color
+
 
 document.body.appendChild(app.view);
-app.renderer.backgroundColor = 0x063639;
+console.log(app.width);
+
+//app.renderer.view.style.position = 200;
+
+//let app = new PIXI.Application({width: 500, height: 50});
+//app.view.style.left = 500;
+//new PIXI.rectangle(10, 10, 10, 10);
+//app.stage
+//find out how to get the width and height of the window ????????????????????????????????????????????
+
+//let sprite = new PIXI.Sprite.fromImage('assets/image.png');
+
+//var h = document.body.clientHeight;
+//var w = document.body.clientWidth;
+
+//window.alert(h);
+
+
+//Add the canvas that Pixi automatically created for you to the HTML document
+
+app.view.style.left = (w/2)-(AppWidth/2);
+app.view.style.top = (h/2)-(AppHeight-(AppHeight/2));
+
+//app.view.style.marginTop = '-('+(AppHeight/2)+')px';
+//app.view.style.top = '50%';
+//app.view.style.y = (h/2);
+//document.window.innerwidth='(w/2)-300';
+
+
+app.renderer.view.style.position = "absolute";
+app.renderer.view.style.display = "block";
+app.renderer.autoResize = true;
+//app.renderer.resize(window.innerWidth, window.innerHeight);
+
+
+
+//app.view.style.position = "relative";
+//app.renderer.autoResize = true;
+
+
 
 
 //resizing the application window
@@ -61,5 +102,3 @@ app.renderer.backgroundColor = 0x063639;
 
 //If I want to change the background color of the application
 //app.renderer.backgroundColor = 0x061639;
-
-let texture = PIXI.utils.TextureCache["cat.png"];
