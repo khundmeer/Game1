7b29f7449162064d6964f6827d921688260db09d
Khundmeer <khundmeerfahad@gmail.com>
2018-07-17 17:00:01 -0500

    destroying objects upon Collision

--- src/main.js
+++ src/main.js
@@ -28,13 +28,64 @@ var Game =
     },
 
     High_Score: 0
+    //var state 
 }
+
 
 var CollisionDetect = 
 {
+    hitRectangle: function (r1, r2) {
 
-
+        //Define the variables we'll need to calculate
+        let hit, combinedHalfWidths, combinedHalfHeights, vx, vy;
+      
+        //hit will determine whether there's a collision
+        hit = false;
+      
+        //Find the center points of each sprite
+        r1.centerX = r1.x + r1.width / 2;
+        r1.centerY = r1.y + r1.height / 2;
+        r2.centerX = r2.x + r2.width / 2;
+        r2.centerY = r2.y + r2.height / 2;
+      
+        //Find the half-widths and half-heights of each sprite
+        r1.halfWidth = r1.width / 2;
+        r1.halfHeight = r1.height / 2;
+        r2.halfWidth = r2.width / 2;
+        r2.halfHeight = r2.height / 2;
+      
+        //Calculate the distance vector between the sprites
+        vx = r1.centerX - r2.centerX;
+        vy = r1.centerY - r2.centerY;
+      
+        //Figure out the combined half-widths and half-heights
+        combinedHalfWidths = r1.halfWidth + r2.halfWidth;
+        combinedHalfHeights = r1.halfHeight + r2.halfHeight;
+      
+        //Check for a collision on the x axis
+        if (Math.abs(vx) < combinedHalfWidths) {
+      
+          //A collision might be occuring. Check for a collision on the y axis
+          if (Math.abs(vy) < combinedHalfHeights) {
+      
+            //There's definitely a collision happening
+            hit = true;
+          } else {
+      
+            //There's no collision on the y axis
+            hit = false;
+          }
+        } else {
+      
+          //There's no collision on the x axis
+          hit = false;
+        }
+      
+        //`hit` will be either `true` or `false`
+        return hit;
+      }
 }
+
 
 //This links pixi to our JS file
 let type = "WebGL"
@@ -102,26 +153,61 @@ let rectangle = new PIXI.Graphics();
 rectangle.beginFill(0x66CCFF);
 rectangle.lineStyle(4, 0xFF3300, 1);
 rectangle.drawRect(0, 0, 33, 33);
+
 //rectangle.drawRect(   x    ,   y      , width, height);
 rectangle.endFill();
 app.stage.addChild(rectangle);
 //Creating a rectangle above.
+
+let rectangle2 = new PIXI.Graphics();
+rectangle2.beginFill(0x66CCFF);
+rectangle2.lineStyle(4, 0xFF3300, 1);
+rectangle2.drawRect(0, 0, 33, 33);
+//rectangle.drawRect(   x    ,   y      , width, height);
+rectangle2.endFill();
+rectangle2.x = AppWidth;
+
+app.stage.addChild(rectangle2);
+
+
+
+
 //-----------------------------------------------------------------------
 
-function setup() {
-
+function setup(hello) 
+{
+    this.id = hello;
     //Start the game loop by adding the `gameLoop` function to
     //Pixi's `ticker` and providing it with a `delta` argument.
     app.ticker.add(delta => gameLoop(delta));
-  }
+}
   
   function gameLoop(delta){
   
     //Move the cat 1 pixel 
-    rectangle.x += 1;
+    rectangle2.x -= 1;
+    
+    if (CollisionDetect.hitRectangle(rectangle,rectangle2))
+        {
+           console.log("We have a hit") ;
+            app.stage.removeChild(rectangle);
+            app.stage.removeChild(rectangle2);
+        
+        }
+    //------------------------------------------------------------------------------------------I stopped here after destroying objects upon collision.
+    
+    //Game.update(delta);
+
+    //if(hitTestRectangle(rectangle,rectangle2))
+   // {
+   //     window.alert("Collision is there");
+//}
+
+
   }
 
-setup();
+var setupObj = new setup('hello world');
+console.log(setupObj);
 
 
 
